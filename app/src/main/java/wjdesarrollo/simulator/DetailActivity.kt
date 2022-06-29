9773d9dc6fd9297e7526925a520a431c0add72ff
unknown <willans@wjdesarrollo-it.uy>
2022-06-29 11:40:13 -0300

    desenvolvimento de layouts de listagem e detalhes de partidas

---
+++ app/src/main/java/wjdesarrollo/simulator/DetailActivity.kt
@@ -0,0 +1,16 @@
+package wjdesarrollo.simulator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import wjdesarrollo.simulator.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_detail)
+    }
+}
--- app/src/main/java/wjdesarrollo/simulator/MainActivity.java
+++ app/src/main/java/wjdesarrollo/simulator/MainActivity.java
@@ -2,6 +2,7 @@ package wjdesarrollo.simulator;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,13 +10,12 @@ import wjdesarrollo.simulator.databinding.ActivityMainBinding;
 
 public class MainActivity<onCreate> extends AppCompatActivity {
 
-    private ActivityMainBinding binding;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        @NonNull ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
     }
 }
